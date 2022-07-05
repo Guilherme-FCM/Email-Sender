32bda6cddd6a321e54246c8111fb5c9e8c0b0904
Guilherme Feitosa <guifcm12@gmail.com>
2022-07-05 17:01:19 -0300

    Implement nodemailer

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,40 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
 
 const app = express()
 app.use(express.json())
 
 app.get('/', (request, response) => {
     return response.json({ message: 'Hello World!' })
+})
+
+app.post('/send-email', (request, response) => {
+    const transporter = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "bd5c48a3368d62",
+          pass: "9e64963176f678"
+        }
+    });
+
+    const message = {
+        from: "sender@server.com",
+        to: "receiver@sender.com",
+        subject: "Message title",
+        text: "Plaintext version of the message",
+        html: "<p>HTML version of the message</p>"
+    }
+
+    transporter.sendMail(message, error => {
+        if (error) return response.status(400).json({
+            error: error.message
+        })
+    })
+
+    return response.json({
+        message: 'Email sended.'
+    })
 })
 
 app.listen(3333, () => {
