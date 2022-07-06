8b9ae8716fb4400da56217cec51c18e53ba23f3a
Guilherme Feitosa <guifcm12@gmail.com>
2022-07-06 10:56:19 -0300

    Add new parameters to request

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,28 @@
 import express, { Request, Response } from 'express'
 import nodemailer from 'nodemailer'
+import Mail from 'nodemailer/lib/mailer'
 import mailConfig from '../config/mail.json'
-import generateMessage from './mail/message'
 
 const app = express()
 app.use(express.json())
 
 app.post('/send-email', (request: Request, response: Response) => {
-    const { email, name } = request.body
-    
-    const mailOptions = generateMessage({ name, email })
+    const { from, to, subject, message, text } = request.body
+
+    if (! from) return response.status(400).json({
+        error: "Sender email (from) is required."
+    }) 
+    if (! to) return response.status(400).json({
+        error: "Recipient email (to) is required."
+    }) 
+    if (! message) return response.status(400).json({
+        error: "A email message is required."
+    }) 
+
+    const mailOptions: Mail.Options = { 
+        from, to, subject, text,
+        html: message
+    }
     const transporter = nodemailer.createTransport(mailConfig);
 
     transporter.sendMail(mailOptions, error => {
